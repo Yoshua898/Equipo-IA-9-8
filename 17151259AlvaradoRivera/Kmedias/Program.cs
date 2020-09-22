8905a1ee0553216a8c84b69a035fbc290bac5750
JosueAlvarado <JosueAlvarado@iqever.com>
2020-09-22 12:57:35 -0500

    no message

---
+++ 17151259AlvaradoRivera/Kmedias/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Kmedias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("El niels es bien perro joto");
+            
+        }
+    }
+}
